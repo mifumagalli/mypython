8f85331013e9c349b55a9cbbbeac246681af78f2
Michele <michele.fumagalli@durham.ac.uk>
2016-10-25 19:01:56 +0100

    tweaked parser to better handle header

--- seds/sb99.py
+++ seds/sb99.py
@@ -14,65 +14,61 @@ class Sb99:
 
         #parse the input file 
         fle=open(path+name+'.input1','r')
+        alllines=[]
+        for ll in fle:
+            alllines.append(ll)
+        fle.close
 
-        dum=fle.readline()
-        hd_name=fle.readline()
-        dum=fle.readline()
-        hd_isf=fle.readline()
-        dum=fle.readline()
-        hd_toma=fle.readline()
-        dum=fle.readline()
-        hd_sfr=fle.readline()
-        dum=fle.readline()
-        hd_ninterv=fle.readline()
-        dum=fle.readline()
-        hd_xponent=fle.readline()
-        dum=fle.readline()
-        hd_xmaslim=fle.readline()
-        dum=fle.readline()
-        hd_sncut=fle.readline()
-        dum=fle.readline()
-        hd_bhcut=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        hd_iz=fle.readline()
-        dum=fle.readline()
-        hd_iwind=fle.readline()
-        dum=fle.readline()
-        hd_time1=fle.readline()
-        dum=fle.readline()
-        hd_jtime=fle.readline()
-        dum=fle.readline()
-        hd_tbiv=fle.readline()
-        dum=fle.readline()
-        hd_itbiv=fle.readline()
-        dum=fle.readline()
-        hd_tmax=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        hd_jmg=fle.readline()
-        dum=fle.readline()
-        hd_lminmax=fle.readline()
-        dum=fle.readline()
-        hd_tdel=fle.readline()
-        dum=fle.readline()
-        hd_iatmos=fle.readline()
-        dum=fle.readline()
-        dum=fle.readline()
-        hd_ilib=fle.readline()
-        dum=fle.readline()
-        hd_iline=fle.readline()
-        dum=fle.readline()
-        hd_ivtrsg=fle.readline()
-        dum=fle.readline()
-        hd_io=fle.readline()
-        fle.close()
-
+        for ii,ll in enumerate(alllines):
+            if('[NAME]' in ll):
+                hd_name=alllines[ii+1].rstrip()
+            if('[ISF]' in ll):
+                hd_isf=alllines[ii+1].rstrip()
+            if('[TOMA]' in ll):
+                hd_toma=alllines[ii+1].rstrip()
+            if('[SFR]' in ll):
+                hd_sfr=alllines[ii+1].rstrip()
+            if('[NINTERV]' in ll):
+                hd_ninterv=alllines[ii+1].rstrip()
+            if('[XPONENT]' in ll):
+                hd_xponent=alllines[ii+1].rstrip()
+            if('[XMASLIM]' in ll):
+                hd_xmaslim=alllines[ii+1].rstrip()
+            if('[SNCUT]' in ll):
+                hd_sncut=alllines[ii+1].rstrip()
+            if('[BHCUT]' in ll):
+                hd_bhcut=alllines[ii+1].rstrip()
+            if('[IZ]' in ll):
+                hd_iz=alllines[ii+7].rstrip()
+            if('[IWIND]' in ll):
+                hd_iwind=alllines[ii+1].rstrip()
+            if('[TIME1]' in ll):
+                hd_time1=alllines[ii+1].rstrip()
+            if('[JTIME]' in ll):
+                hd_jtime=alllines[ii+1].rstrip()
+            if('[TBIV]' in ll):
+                hd_tbiv=alllines[ii+1].rstrip()
+            if('[ITBIV]' in ll):
+                hd_itbiv=alllines[ii+1].rstrip()
+            if('[TMAX]' in ll):
+                hd_tmax=alllines[ii+1].rstrip()
+            if('[JMG]' in ll):
+                hd_jmg=alllines[ii+1].rstrip()
+            if('[LMIN,' in ll):
+                hd_lminmax=alllines[ii+1].rstrip()
+            if('[TDEL]' in ll):
+                hd_tdel=alllines[ii+1].rstrip()
+            if('[IATMOS]' in ll):
+                hd_iatmos=alllines[ii+1].rstrip()
+            if('[ILIB]' in ll):
+                hd_ilib=alllines[ii+2].rstrip()
+            if('[ILINE]' in ll):
+                hd_iline=alllines[ii+1].rstrip()
+            if('[IVT,' in ll):
+                hd_ivtrsg=alllines[ii+1].rstrip()
+            if('[IO1,' in ll):
+                hd_io=alllines[ii+1].rstrip()
+        
         #store header
         self.header={"NAME":hd_name,"ISF":hd_isf,"TOMA":hd_toma,
                      "SFR":hd_sfr,"NINTERV":hd_ninterv,"XPONENT":hd_xponent,
@@ -82,13 +78,12 @@ class Sb99:
                      "TMAX":hd_tmax,"JMG":hd_jmg,"LMINMAX":hd_lminmax,
                      "TDEL":hd_tdel,"IATMOS":hd_iatmos,"ILIB":hd_ilib,
                      "ILINE":hd_iline,"IVTRSG":hd_ivtrsg,"IO":hd_io}
-
         
         #parse other files 
         files=(self.header["IO"].rstrip()).split(",")
-
+        
         #quanta file
-        if(files[0] == '1'):
+        if(files[0] == '+1'):
             
             fle=open(path+name+'.quanta1','r')
             
@@ -109,11 +104,9 @@ class Sb99:
                     LogL.append(float(columns[7]))
             fle.close()
             self.quanta={"TIME":time,"HI":HI,"HeI":HeI,"HeII":HeII,"LogL":LogL}
-
         
-
         #spectra file
-        if(files[6] == '1'):
+        if(files[6] == '+1'):
             
             fle=open(path+name+'.spectrum1','r')
             
@@ -147,7 +140,6 @@ class Sb99:
 
             self.spectrum=[]
             for tt in untimes:
-
                 nzero=np.nonzero( abs(time-tt) < 1)
                 nzero=nzero[0]
                 self.spectrum.append({"TIME":time[nzero],"WAVE":wavelength[nzero],"TOTAL":total[nzero],
