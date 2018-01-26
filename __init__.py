3014312f932c128d0fa35ca9acc8ca2b290a8330
Michele <michele.fumagalli@durham.ac.uk>
2018-01-26 10:23:06 +0000

    small tweaks to mask code including indent fixing

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
-import dust 
-import filters 
-import fits
-import seds
-import ramses
-import ifu
-import redshifts
-import lumfunc
-import cosmo
-import spectra
-import radtrans
+try:
+    import dust 
+    import filters 
+    import fits
+    import seds
+    import ramses
+    import ifu
+    import redshifts
+    import lumfunc
+    import cosmo
+    import spectra
+    import radtrans
+except:
+    pass
--- fits/pyregmask.py
+++ fits/pyregmask.py
@@ -2,40 +2,43 @@ import argparse
 try:
     import pyregion as pyreg
 except:
-    print 'Cannot import pyregion'
+    print('Cannot import pyregion')
 import numpy as np
 
 class PyMask(object):
-    
-    """Class to handle masks in fits files """
+
+    """ Class to handle masks in fits files """
 
     def __init__(self, x, y, regfile, header=None):
+
+        """
+        Parse the ds9 region file here. Also save the number 
+	of regions and the x y dimensions of the data
+        """
         
-	""" Parse the ds9 region file here. Also save the number 
-	of regions and the x y dimensions of the data"""
-	if(header):
+        if(header):
             self.reg = pyreg.open(regfile).as_imagecoord(header=header)
         else:
             self.reg = pyreg.open(regfile)
 
-	self.filter = self.reg.get_filter()
-	self.x = x
-	self.y = y
-	self.nreg = len(self.reg)
+
+        self.filter = self.reg.get_filter()
+        self.x = x
+        self.y = y
+        self.nreg = len(self.reg)
     
     def fillmask(self, ind):
         
-	""" Given a region object and an index fill the mask"""
+        """ Given a region object and an index fill the mask"""
+
+        if (self.reg[ind]).coord_format == 'image':
 	
-	if (self.reg[ind]).coord_format == 'image':
-	
-	  self.mask = self.filter[ind].mask((self.y, self.x))
-          self.maskname = (self.reg[ind]).name
-	  self.maskcomm = (self.reg[ind]).comment
-	
-	else:
-	 
-	 raise TypeError('The region coordinates are not in image units')
+            self.mask = self.filter[ind].mask((self.y, self.x))
+            self.maskname = (self.reg[ind]).name
+            self.maskcomm = (self.reg[ind]).comment
+
+        else:
+            raise TypeError('The region coordinates are not in image units')
 	
     def write(self,outname):
         """Write the mask output"""
@@ -46,7 +49,7 @@ class PyMask(object):
 	#Mask is a boolean --> convert on the fly
         hdu = fits.PrimaryHDU(self.mask*1)
         hdu.header['REGTYPE'] = self.maskname
-	hdu.header['REGNAME'] = self.maskcomm
+        hdu.header['REGNAME'] = self.maskcomm
 	
 	#write
         hdu.writeto(outname,clobber=True)
